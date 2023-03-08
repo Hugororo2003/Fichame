39efc5d92d49b37bc427cf320538f1c42a127324
ZIMOS10 <vespin@ieshlanz.es>
2023-03-08 20:50:58 +0100

    logo en todas las vistas

---
+++ Events/src/Entity/Images.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImagesRepository::class)]
+class Images
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ruta = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRuta(): ?string
+    {
+        return $this->ruta;
+    }
+
+    public function setRuta(string $ruta): self
+    {
+        $this->ruta = $ruta;
+
+        return $this;
+    }
+}
